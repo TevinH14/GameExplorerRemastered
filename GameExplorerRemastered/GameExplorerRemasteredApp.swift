601432111dd5b82ad521bc1fb0ec957f0126f2da
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2025-07-24 20:56:27 -0500

    Initial Commit

---
+++ GameExplorerRemastered/GameExplorerRemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameExplorerRemasteredApp.swift
+//  GameExplorerRemastered
+//
+//  Created by Tevin on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GameExplorerRemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
