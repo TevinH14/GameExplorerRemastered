37c7ad5dc3bb14c2f78b425657aa532ad8bcd318
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2025-08-13 22:47:12 -0500

    GameRepository main api call for games
    added base GameRepository to make calls for games.

---
+++ GameExplorerRemastered/Services/GameServices/GameRepository.swift
@@ -0,0 +1,21 @@
+////
+////  GameRepository.swift
+////  GameExplorerRemastered
+////
+////  Created by Tevin on 7/29/25.
+////
+//
+//import Foundation
+//
+//struct GameRepository: GameRepositoryProtocol {
+//    private let networkService = NetworkService()
+//    private let jsonDecoder = JSONDecoder()
+//    
+//    func fetchGames(with urlString: String, query: String) async throws -> [Game]? {
+//        <#code#>
+//    }
+//    
+//    func getSearchedGame(with searchString: String) async throws -> [Game]? {
+//        <#code#>
+//    }
+//}
