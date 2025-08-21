e964eaca0c1eacc1a44769a019c1e69654dfe67c
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2025-08-20 22:13:10 -0500

    Create GameCarousel.swift
    move carousel to game dic

---
+++ GameExplorerRemastered/Features/Games/Views/GameCarousel.swift
@@ -0,0 +1,44 @@
+//
+//  GameCarousel.swift
+//  GameExplorerRemastered
+//
+//  Created by Tevin on 7/29/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct GameCarousel: View {
+    
+    let header: String
+    let gameList: [Game]
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 16) {
+                GameText(text: header, font: .title, alignment: .leading, width: UIScreen.screenWidth / 1.1)
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .fontWeight(.heavy)
+            }
+            ScrollView(.horizontal,  showsIndicators: false) {
+                HStack(spacing: 24) {
+                    ForEach(gameList) { gameItem in
+                        NavigationLink {
+                            //UPDATE: game detail view
+                        } label: {
+                            GameItem(gameItem: gameItem)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct GameCarouselPreview: PreviewProvider {
+    static var previews: some View{
+        GameCarousel(header: "Trending", gameList: [.preview, .preview])
+    }
+}
