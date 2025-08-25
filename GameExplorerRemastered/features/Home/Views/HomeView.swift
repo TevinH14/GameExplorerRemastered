79d2872ad1b49d3aee512c097198a0afafb88390
TevinH14 <40679667+TevinH14@users.noreply.github.com>
2025-08-24 21:07:00 -0400

    Create HomeView.swift
    move view inside views folder for home view.

---
+++ GameExplorerRemastered/features/Home/Views/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeView.swift
+//  GameExplorerRemastered
+//
+//  Created by Tevin on 8/19/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView<homeViewModel: HomeViewModelProtocol>: View {
+    @ObservedObject var viewModel: homeViewModel
+    var body: some View {
+        ZStack {
+            if (viewModel.gameList.isEmpty) {
+                
+            }
+            else {
+                VStack {
+                    if (viewModel.gameList.isEmpty) {
+                        ProgressView()
+                    } else {
+                        GameCarousel(header: "games", gameList: viewModel.gameList)
+                        
+                    }
+                }
+            }
+        }
+        .onAppear() {
+            viewModel.fetchGames()
+        }
+    }
+}
+
+
+//MARK: Home View Preview
+struct HomeView_Previews: PreviewProvider {
+    class MockHomeViewModel: HomeViewModelProtocol {
+        @Published var repository: any GameRepositoryProtocol = MockGameRepository()
+        @Published var gameList: [Game] = [.preview, .preview]
+        
+        func fetchGames() {}
+    }
+    
+    static var previews: some View {
+        HomeView(viewModel: MockHomeViewModel())
+    }
+}
