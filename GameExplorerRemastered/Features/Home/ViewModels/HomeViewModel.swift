07b85d3adceab66aa2266291f0f30e28607b0907
TevinH14 <40679667+TevinH14@users.noreply.github.com>
2025-10-29 15:09:32 -0500

    Update HomeViewModel.swift
    update using game response

--- GameExplorerRemastered/Features/Home/ViewModels/HomeViewModel.swift
+++ GameExplorerRemastered/Features/Home/ViewModels/HomeViewModel.swift
@@ -11,21 +11,64 @@ import SwiftUI
 
 @MainActor
 final class HomeViewModel: HomeViewModelProtocol {
-    var repository: any GameRepositoryProtocol
-    @Published var gameList: [Game]
+    @Published var isLoaded: Bool
+        
+    var repository: GameRepositoryProtocol
     
-    init(repository: any GameRepositoryProtocol) {
+    @Published var trendingResponse: GameResponse?
+    @Published var upcomingResponse: GameResponse?
+    @Published var recentReleasedResponse: GameResponse?
+    
+    @Published var trendingList: [Game]
+    @Published var upcomingList: [Game]
+    @Published var recentReleasedList: [Game]
+
+    init(repository: GameRepositoryProtocol) {
+        self.isLoaded = false
         self.repository = repository
-        self.gameList = []
+        self.trendingList = []
+        self.upcomingList = []
+        self.recentReleasedList = []
+
     }
     
-    func fetchGames() async {
+    //MARK: - Fetch Games HomeViewModel
+    func fetchGames() {
         Task {
             do {
-                if let games = try await self.repository.getGames() {
-                    self.gameList = games
+                
+                //fetch trending games
+                if let tendingGames = try await self.repository.getTrendingGames() {
+                    self.trendingResponse = tendingGames
+                    self.trendingList = tendingGames.results
+                    print("HomeViewModel: is trendingList empty \(trendingList.isEmpty)")
+
                 }
-               
+                
+                // fetch upcoming games
+                if let upcomingGames = try await self.repository.getUpcoming30DaysGames() {
+                    self.upcomingResponse = upcomingGames
+
+                    self.upcomingList = upcomingGames.results
+                    print("HomeViewModel: is upcomingList empty \(upcomingList.isEmpty)")
+
+                }
+                
+                //fetch last 30 days games // recently released
+                if let recentReleasedsGames = try await self.repository.getLast30DaysGames() {
+                    self.recentReleasedResponse = recentReleasedsGames
+                    self.recentReleasedList = recentReleasedsGames.results
+                    print("HomeViewModel: is recentReleasedList empty \(recentReleasedList.isEmpty)")
+
+                }
+                
+                    
+                if (!trendingList.isEmpty && !upcomingList.isEmpty && !recentReleasedList.isEmpty) {
+                    isLoaded = true
+                    print("HomeViewModel: is loaded \(isLoaded)")
+
+                }
+                
             } catch {
                 print(error.localizedDescription)
                 
