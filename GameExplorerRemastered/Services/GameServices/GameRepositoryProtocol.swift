be1220a7ad425e18d61148098770cecb5c2b76e4
TevinH14 <40679667+TevinH14@users.noreply.github.com>
2025-10-29 15:12:10 -0500

    update to return response

--- GameExplorerRemastered/Services/GameServices/GameRepositoryProtocol.swift
+++ GameExplorerRemastered/Services/GameServices/GameRepositoryProtocol.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 protocol GameRepositoryProtocol {
-    func getGames() async throws -> [Game]?
+    func getGames() async throws -> GameResponse?
+    func getTrendingGames() async throws -> GameResponse?
+    func getLast30DaysGames() async throws -> GameResponse?
+    func getUpcoming30DaysGames() async throws -> GameResponse?
+    func getTopOfAllTimeGames() async throws -> GameResponse?
+    func getLastYearPopularGames() async throws -> GameResponse?
 //    func getSearchedGame(with searchString: String ) async throws -> [Game]?
 
     
